8c2708734a2568b31afabb5afcf92e5e9f078f97
Pál Kecskeméti <pal.kecskemeti@p92.hu>
2022-02-22 10:57:47 +0100

    Refactoring InputText

--- src/com/morganstanley/rolingstrings2/InputText.java
+++ src/com/morganstanley/rolingstrings2/InputText.java
@@ -15,38 +15,22 @@ public class InputText {
 	public char[] rotateLeft() {
 		StringBuilder result = new StringBuilder();
 		char[] theAlphabet = AlphabetGeneratorUtil.getAlphabet();
-		if ( startIndex != 0 ) {
-			for ( int index = 0; index < startIndex; index++ ) {
-				result.append( inputChars[ index ] );
-			}
-		}
-		for ( int index = startIndex; index <= endIndex; index++ ) {
-			char theChar = inputChars[ index ];
-			int originalAlphabetPosition = theChar - 'a';
-			char newChar;
-			if ( originalAlphabetPosition == 0 ) {
-				newChar = theAlphabet[ theAlphabet.length - (originalAlphabetPosition + 1) ];
-			} else {
-				newChar = theAlphabet[ originalAlphabetPosition - 1 ];
-			}
-			result.append( newChar );
-		}
-		if ( endIndex != inputChars.length - 1 ) {
-			for ( int index = endIndex + 1; index < inputChars.length; index++ ) {
-				result.append( inputChars[ index ] );
-			}
-		}
+		appendToResultTheFirstPartNotChangedChars( result );
+		appendToResultChangedLeftCharacters( result, theAlphabet );
+		appendToResultTheLastPartNotChangedChars( result );
 		return result.toString().toCharArray();
 	}
 
 	public char[] rotateRight() {
 		StringBuilder result = new StringBuilder();
 		char[] theAlphabet = AlphabetGeneratorUtil.getAlphabet();
-		if ( startIndex != 0 ) {
-			for ( int index = 0; index < startIndex; index++ ) {
-				result.append( inputChars[ index ] );
-			}
-		}
+		appendToResultTheFirstPartNotChangedChars( result );
+		appendToResultChangedRightCharacters( result, theAlphabet );
+		appendToResultTheLastPartNotChangedChars( result );
+		return result.toString().toCharArray();
+	}
+
+	private void appendToResultChangedRightCharacters( StringBuilder result, char[] theAlphabet ) {
 		for ( int index = startIndex; index <= endIndex; index++ ) {
 			char theChar = inputChars[ index ];
 			int originalAlphabetPosition = theChar - 'a';
@@ -58,12 +42,35 @@ public class InputText {
 			}
 			result.append( newChar );
 		}
+	}
+
+	private void appendToResultChangedLeftCharacters( StringBuilder result, char[] theAlphabet ) {
+		for ( int index = startIndex; index <= endIndex; index++ ) {
+			char theChar = inputChars[ index ];
+			int originalAlphabetPosition = theChar - 'a';
+			char newChar;
+			if ( originalAlphabetPosition == 0 ) {
+				newChar = theAlphabet[ theAlphabet.length - (originalAlphabetPosition + 1) ];
+			} else {
+				newChar = theAlphabet[ originalAlphabetPosition - 1 ];
+			}
+			result.append( newChar );
+		}
+	}
+
+	private void appendToResultTheFirstPartNotChangedChars( StringBuilder result ) {
+		if ( startIndex != 0 ) {
+			for ( int index = 0; index < startIndex; index++ ) {
+				result.append( inputChars[ index ] );
+			}
+		}
+	}
+
+	private void appendToResultTheLastPartNotChangedChars( StringBuilder result ) {
 		if ( endIndex != inputChars.length - 1 ) {
 			for ( int index = endIndex + 1; index < inputChars.length; index++ ) {
 				result.append( inputChars[ index ] );
 			}
 		}
-		return result.toString().toCharArray();
 	}
-
 }
